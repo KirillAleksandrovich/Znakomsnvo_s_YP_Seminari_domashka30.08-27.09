70fec6987ad7a9d210b40c381ce68603ab1742c4
«Kirill» <kirill.feshev@bk.ru>
2022-09-03 23:33:42 +0300

    add Task 04

---
+++ Task04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу. 
+// Которая принимает на вход три числа. 
+// И выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.WriteLine("Привет! Как тебя зовут? ");
+String username = Console.ReadLine();
+Console.Write($"{username}, введи первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Теперь, введи второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Отлично и ещё введи третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+Console.Write($"Смотри {username}, {max} получается больше всех :)");
+Console.Write($"Как говорила моя бабушка: Ученье свет! А не ученье, как свет так на работу.");
