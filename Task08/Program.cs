7c70b91b11b26b67e05d8d9ea4dda7de954916ab
«Kirill» <kirill.feshev@bk.ru>
2022-09-04 15:56:07 +0300

    add Task 08

---
+++ Task08/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 8: 
+// Напишите программу. 
+// Которая на вход принимает число (N).
+// А на выходе показывает все чётные
+// числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Clear();
+Console.WriteLine("Добро пожаловать путник! Как твоё имя?  ");
+String username = Console.ReadLine();
+
+Console.WriteLine($"Я ждал тебя {username}!");
+
+Console.WriteLine($"Сейчас я разложу твоё число на чётные числа. Как всегда вводи число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (num > 1 && count != num)
+{
+    if (count%2 == 0)
+    {
+        count = count + 2;
+        Console.Write($"{count}, ");
+    }
+    else{
+        count = count ++;
+    }
+    num = num -2;
+}
